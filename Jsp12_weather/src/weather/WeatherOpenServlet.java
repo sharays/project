ec9f88d9573d712f4b60185be250ed945b8201de
hk_edu14 <hk_edu14@192.168.3.123>
2017-12-06 13:41:57 +0900

    first commit

---
+++ Jsp12_weather/src/weather/WeatherOpenServlet.java
@@ -0,0 +1,52 @@
+package weather;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WeatherOpenServlet
+ */
+@WebServlet("/weatherOpen")
+public class WeatherOpenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WeatherOpenServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request,response);
+	}
+
+	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		String code = request.getParameter("code");
+		request.setAttribute("code",code);
+		RequestDispatcher rd = request.getRequestDispatcher("weatherInfo.jsp");
+		rd.forward(request, response);
+		
+	}
+
+}
